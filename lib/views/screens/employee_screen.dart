7d787cbdca6feccfcd5eddb9678c65afd5bc3a81
adogbeji <baka_1993@yahoo.co.uk>
2025-04-05 10:01:56 +0100

    Add starting files for employee screen

---
+++ lib/views/screens/employee_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class EmployeeScreen extends StatefulWidget {
+  const EmployeeScreen({super.key});
+
+  @override
+  State<EmployeeScreen> createState() => _EmployeeScreenState();
+}
+
+class _EmployeeScreenState extends State<EmployeeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Employee Screen'),
+      ),
+    );
+  }
+}
