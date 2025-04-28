df9074612fc6a4f1899dc3228cf8859fd77bc308
adogbeji <baka_1993@yahoo.co.uk>
2025-04-28 14:30:41 +0100

    Add code to navigate from home screen --> employees screen & add app bar to employee screen

--- lib/views/screens/employee_screen.dart
+++ lib/views/screens/employee_screen.dart
@@ -11,6 +11,29 @@ class _EmployeeScreenState extends State<EmployeeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Employee',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Form',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.orange,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Container(
         child: const Column(
           children: [],
--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/views/screens/employee_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -40,7 +41,9 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const EmployeeScreen(),),);
+        },
         child: const Icon(Icons.add),
       ),
     );
